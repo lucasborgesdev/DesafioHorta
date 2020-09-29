840872a95af6992069600e2c7c4299d27fa2d93b
Lucas Borges <45474687+lucasborgesdev@users.noreply.github.com>
2020-09-28 22:24:45 -0300

    criado classe robo

---
+++ Robo.class.js
@@ -0,0 +1,68 @@
+export default class Robo {
+  constructor(posicao, direcao) {
+    this.posicao = posicao;
+    this.direcao = direcao;
+  }
+
+  virar_para(nova_direcao) {
+    if (this.direcao == "S") {
+      if (nova_direcao == "S") {
+        return "";
+      }
+      if (nova_direcao == "O") {
+        return "D";
+      }
+      if (nova_direcao == "L") {
+        return "E";
+      }
+      if (nova_direcao == "N") {
+        return "EE"; // gira 180 graus, tanto faz a direção
+      }
+    }
+
+    if (this.direcao == "O") {
+      if (nova_direcao == "S") {
+        return "E";
+      }
+      if (nova_direcao == "O") {
+        return "";
+      }
+      if (nova_direcao == "L") {
+        return "EE"; // gira 180 graus, tanto faz a direção
+      }
+      if (nova_direcao == "N") {
+        return "D";
+      }
+    }
+
+    if (this.direcao == "L") {
+      if (nova_direcao == "S") {
+        return "D";
+      }
+      if (nova_direcao == "O") {
+        return "EE"; // gira 180 graus, tanto faz a direção
+      }
+      if (nova_direcao == "L") {
+        return "";
+      }
+      if (nova_direcao == "N") {
+        return "E";
+      }
+    }
+
+    if (this.direcao == "N") {
+      if (nova_direcao == "S") {
+        return "EE"; // gira 180 graus, tanto faz a direção
+      }
+      if (nova_direcao == "O") {
+        return "E";
+      }
+      if (nova_direcao == "L") {
+        return "D";
+      }
+      if (nova_direcao == "N") {
+        return "";
+      }
+    }
+  }
+}
